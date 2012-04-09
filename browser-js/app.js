3921b6aca0fb171c6d37e15c5b626be78835696c
Dallon Feldner <dallonfdev@gmail.com>
2012-04-09 11:12:05 -0700

    Added documentation link

--- browser-js/app.js
+++ browser-js/app.js
@@ -2,6 +2,26 @@ var App = Backbone.Model.extend({
   defaults: {
     appName: 'My App',
     appUrl: ''
+  },
+
+  initialize: function() {
+    this.on('change:resourceTypeId', this.setDocLink, this);
+    this.setDocLink();
+  },
+
+  setDocLink: function() {
+    var type = this.get('resourceTypeId');
+    var url = 'http://deployd.github.com/deployd/';
+    console.log(type);
+    if (type === 'Static') {
+      url += 'files.html'
+    } else if (type === 'Collection') {
+      url += 'collection.html'
+    } else if (type === 'UserCollection') {
+      url += 'usercollection.html'
+    }
+
+    this.set('documentation', url);
   }
 });
 
