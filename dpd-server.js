75bd67b219b309d3989208494c2f0326501358c9
DallonF <dallonfdev@gmail.com>
2012-05-15 10:05:42 -0700

    Fixed typo in sync modal

--- dpd-server.js
+++ dpd-server.js
@@ -3,6 +3,7 @@ var dpd = require('deployd');
 // Start testing server
 dpd.use('http://localhost:2403').listen(function () {
   dpd.use('/keys').post(require('deployd/lib/key').keygen(), function (err, k) {
+    if (err) { return console.log(err); }
     process.send(k);
   })
 });
